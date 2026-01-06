d457e2ef892714373a349202cf2b95818696f164
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-01-06 22:47:19 +0300

    Web and Mobile projects separated

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import { projects } from "../utils/project_data";
 
 function Projects() {
+  const web_projects = projects.filter((item) => item.platform !== "mobile");
+  const mobile_projects = projects.filter((item) => item.platform === "mobile");
   return (
     <div className="max-w-6xl mx-auto p-6 space-y-8">
-      <Title text="My Projects" />
+      <Title text="Web Projects" />
       <div className="bg-black text-white p-4 mx-auto text-center">
         <p className="text-gray-300 text-sm">
           Most of my work has been done for companies or clients, so I’m only
@@ -15,7 +17,19 @@ function Projects() {
         </p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((project) => (
+        {web_projects.map((project) => (
+          <Link key={project.id} href={`/projects/${project.slug}`}>
+            <ProjectCard
+              title={project.title}
+              description={project.description}
+              imageUrl={project.imageUrl}
+            />
+          </Link>
+        ))}
+      </div>
+      <Title text="Mobile Projects (React Native)" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {mobile_projects.map((project) => (
           <Link key={project.id} href={`/projects/${project.slug}`}>
             <ProjectCard
               title={project.title}
